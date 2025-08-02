415863124794a0d6e73f322f9797ee976b0a17ff
alinassarrr <aliinassar8@gmail.com>
2025-08-02 22:58:20 +0300

    feat(ItemCard): Enhance ItemCard component to utilize props for dynamic content and implement add to cart functionality

--- frontend/src/components/ItemCard/index.jsx
+++ frontend/src/components/ItemCard/index.jsx
@@ -1,29 +1,34 @@
 import React from "react";
 import "./styles.css";
 import { FaCartArrowDown } from "react-icons/fa";
+import { useCart } from "../../contexts/CartContext";
 
-const ItemCard = ({ img, title, description, price }) => {
+const ItemCard = ({ id, img, title, description, price }) => {
+  const { addToCart } = useCart();
+
+  const handleAddToCart = () => {
+    const product = {
+      id: id,
+      img,
+      title,
+      description,
+      price,
+    };
+    addToCart(product);
+  };
+
   return (
     <div className="item-card">
       <div className="image-wrapper">
-        <img src={img} alt="Product" />
+        <img src={img} alt={title} />
       </div>
       <div className="cardinfo-wrapper">
-        <p className="card-title">Item Title</p>
-        <p className="card-desc">
-          Desc Lorem ipsum dolor sit amet, consectetur adipisicing Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Quisquam deserunt
-          explicabo asperiores, corrupti mollitia illo velit? Consequuntur saepe
-          itaque repellendus, velit inventore, tempore tenetur ea delectus,
-          libero obcaecati maiores consectetur?
-        </p>
+        <p className="card-title">{title}</p>
+        <p className="card-desc">{description}</p>
         <div className="price-add">
-          <p className="price">$29.99</p>
+          <p className="price">${price}</p>
 
-          <button
-            className="add-to-cart"
-            onClick={() => console.log("clicked")}
-          >
+          <button className="add-to-cart" onClick={handleAddToCart}>
             <FaCartArrowDown />
             <p>Add</p>
           </button>
