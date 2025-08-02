5bdc20338accfc1a9996858c4e8c18aa2a43fc93
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-08-02 13:37:33 +0300

    feat(App): Add Admin routes and components for enhanced admin functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter} from 'react-router-dom';
-import App from './App.jsx';
-import { UserProvider } from './contexts/UserContext';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import { UserProvider } from "./contexts/UserContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <UserProvider>
       <BrowserRouter>
@@ -12,4 +12,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </UserProvider>
   </StrictMode>
-)
+);
--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -2,10 +2,10 @@ import Navbar from "../../components/Navbar";
 
 const HomePage = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <h1 style={{ textAlign: "center" }}>Welcome to Home Page</h1>
-    </div>
+    </>
   );
 };
 
