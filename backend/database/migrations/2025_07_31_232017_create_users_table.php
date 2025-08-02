6ecdd4a0efc0b30658aaec3d74d9d94a8262c326
abedb961 <abedbaydoun961@hotmail.com>
2025-08-02 06:50:37 +0300

    fix(migrations): Remove address calumn

--- backend/database/migrations/2025_07_31_232017_create_users_table.php
+++ backend/database/migrations/2025_07_31_232017_create_users_table.php
@@ -11,10 +11,10 @@ return new class extends Migration {
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('profile_picture_url')->nullable();
             $table->string('phone_number')->nullable();
-            $table->string('address')->nullable();
             $table->date('date_of_birth')->nullable();
             $table->rememberToken();
             $table->timestamps();
