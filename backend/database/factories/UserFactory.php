04be25e846951bc63defc83b15dd7f8bcd0ede99
abedb961 <abedbaydoun961@hotmail.com>
2025-08-02 06:45:39 +0300

    fix(factories): Link user factory to user model

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -5,27 +5,23 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory {
-    protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected static ?string $password;
+    protected $model = User::class;
+
+
     public function definition(): array {
         return [
             'first_name' => fake()->name(),
             'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'profile_picture_url' => fake()->imageUrl(640, 480, 'people'),
             'phone_number' => fake()->phoneNumber(),
-            'address' => fake()->address(),
             'date_of_birth' => fake()->date(),
             'remember_token' => Str::random(10),
         ];
