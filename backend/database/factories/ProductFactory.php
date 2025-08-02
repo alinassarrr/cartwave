3c16e84e6d44da80d2a75f2776fa27fd46e9e60a
abedb961 <abedbaydoun961@hotmail.com>
2025-08-02 06:34:06 +0300

    fix(factories): Link category factory to category model

--- backend/database/factories/ProductFactory.php
+++ backend/database/factories/ProductFactory.php
@@ -3,16 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
- */
 class ProductFactory extends Factory {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
+    protected $model = Product::class;
+
     public function definition(): array {
         return [
             'name' => $this->faker->words(3, true),
