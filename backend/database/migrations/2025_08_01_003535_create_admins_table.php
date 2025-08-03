808933df93b44d68493f5c49ac4e6aa503a70e0d
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 00:36:06 +0300

    fix(migrations): Add foreign key to admins table

--- backend/database/migrations/2025_08_01_003535_create_admins_table.php
+++ backend/database/migrations/2025_08_01_003535_create_admins_table.php
@@ -11,6 +11,7 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
