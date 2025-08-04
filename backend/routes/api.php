ebd40b74cdf3f3efd7cc8adae305cffec0bd57aa
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 05:53:29 +0300

    back(routes): Add auth routes

---
+++ backend/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Common\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/password/email', [AuthController::class, 'sendResetLinkEmail']);
+    Route::post('/password/reset', [AuthController::class, 'reset']);
+});
+
+Route::middleware('auth:api')->prefix('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+});
