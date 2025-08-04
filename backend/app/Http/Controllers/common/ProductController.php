158f02cea75d3f3c73541d4e41e5c22351854056
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 06:14:53 +0300

    back(controllers): Refactor product controller

---
+++ backend/app/Http/Controllers/common/ProductController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Common;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Product\FilterProductRequest;
+use App\Services\ProductService;
+
+class ProductController extends Controller {
+    public function index(FilterProductRequest $request) {
+        $products = ProductService::getFilteredProducts($request->validated());
+        return $this->responseJSON($products);
+    }
+
+    public function show($id) {
+        $product = ProductService::getProductById($id);
+        return $this->responseJSON($product);
+    }
+}
