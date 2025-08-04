7e5abd0a99ff73ab4fec3495298b591537c9d401
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 06:54:20 +0300

    back(traits): Add conditional to prevent double wrapping

--- backend/app/Traits/RespondWithJson.php
+++ backend/app/Traits/RespondWithJson.php
@@ -4,6 +4,10 @@ namespace App\Traits;
 
 trait RespondsWithJson {
     public function responseJSON($data = null, $status = 'success', $code = 200) {
+        if ($data instanceof \Illuminate\Http\JsonResponse) {
+            return $data;
+        }
+
         return response()->json([
             'status' => $status,
             'data' => $data,
