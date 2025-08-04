b602b6a4710b614530d92a65ccf22be52b38147d
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 03:17:20 +0300

    back(models): Add relationships to order model

--- backend/app/Models/Order.php
+++ backend/app/Models/Order.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Order extends Model {
     
     use HasFactory;
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items() {
+        return $this->hasMany(OrderItem::class);
+    }
 }
