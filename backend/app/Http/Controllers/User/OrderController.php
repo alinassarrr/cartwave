1c6e9c09e55069f794439cc013699d69ba746f1e
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 07:58:04 +0300

    back(controllers): Add order controller

---
+++ backend/app/Http/Controllers/User/OrderController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Services\OrderService;
+use Illuminate\Support\Facades\Auth;
+
+class OrderController extends Controller {
+    public function index() {
+        $orders = OrderService::getOrdersForUser(Auth::id());
+        return $this->responseJSON($orders);
+    }
+
+    public function show($id) {
+        $order = OrderService::getSingleOrderForUser(Auth::id(), $id);
+        if (!$order) {
+            return $this->responseJSON(null, 'error', 404);
+        }
+        return $this->responseJSON($order);
+    }
+}
