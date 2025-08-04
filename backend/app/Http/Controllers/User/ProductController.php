7163d74f515c9bbdf929818377dbb41666f6d6d3
abedb961 <abedbaydoun961@hotmail.com>
2025-08-04 08:52:57 +0300

    back(requests): Add address request

--- backend/app/Http/Controllers/User/ProductController.php
+++ backend/app/Http/Controllers/User/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Product\FilterProductRequest;
+use App\Http\Requests\User\FilterProductRequest;
 use App\Services\User\ProductService;
 
 class ProductController extends Controller {
