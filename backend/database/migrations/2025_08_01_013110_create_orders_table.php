ac5f30a603b49b3ea01749b00e3e2f7f49af24ca
abedb961 <abedbaydoun961@hotmail.com>
2025-08-05 05:07:45 +0300

    fix(migrations): Add order number to orders table

--- backend/database/migrations/2025_08_01_013110_create_orders_table.php
+++ backend/database/migrations/2025_08_01_013110_create_orders_table.php
@@ -9,6 +9,7 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_number')->unique();
             $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->decimal('total', 10, 2);
             $table->decimal('shipping_price', 10, 2)->default(0.00);
