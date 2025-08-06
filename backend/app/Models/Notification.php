4c3070a25045490a261063379a85f69317a06a66
salembeyrouti <salem_beyrouti11@hotmail.com>
2025-08-07 00:38:33 +0300

    feat(Notification): Modify the notification Model

--- backend/app/Models/Notification.php
+++ backend/app/Models/Notification.php
@@ -11,9 +11,16 @@ class Notification extends Model{
 
     protected $fillable = [
         'user_id',
+        'title',
         'message',
+        'type',
         'is_read',
         'read_at',
+    ];
+
+     protected $casts = [
+        'is_read' => 'boolean',
+        'read_at' => 'datetime',
     ];
 
     public function user() {
